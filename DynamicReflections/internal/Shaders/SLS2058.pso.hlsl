7c030414c105ed2489374108ddd56bcb84ad9f90
Wall SoGB <11296448+WallSoGB@users.noreply.github.com>
2023-09-08 07:52:51 +0200

    Update shaders, fix SM3.0 one

--- DynamicReflections/internal/Shaders/SLS2058.pso.hlsl
+++ DynamicReflections/internal/Shaders/SLS2058.pso.hlsl
@@ -2,11 +2,11 @@
 //
 // Parameters:
 
-float4      AmbientColor        : register(c1);
-sampler2D   CustomEnvMask       : register(s3);
-float4      EnvToggles          : register(c27);
-samplerCUBE EnvironmentCubeMap  : register(s1);
 sampler2D   NormalMap           : register(s0);
+samplerCUBE EnvironmentCubeMap  : register(s1);
+sampler2D   CustomEnvMask       : register(s3);
+float4      AmbientColor        : register(c1);
+float4      EnvToggles          : register(c27);
 
 
 // Registers:
@@ -24,16 +24,16 @@ sampler2D   NormalMap           : register(s0);
 // Structures:
 
 struct VS_INPUT {
-    float4  texcoord_1   : TEXCOORD1_centroid;
-    float4  texcoord_2   : TEXCOORD2_centroid;
-    float4  texcoord_3   : TEXCOORD3_centroid;
-    float2  NormalUV     : TEXCOORD0;
-    float3  VertexColor  : COLOR0;
-    float   texcoord_4   : TEXCOORD4;
+    float4 texcoord_1       : TEXCOORD1_centroid;
+    float4 texcoord_2       : TEXCOORD2_centroid;
+    float4 texcoord_3       : TEXCOORD3_centroid;
+    float2 tcCoord          : TEXCOORD0;
+    float3 vVertexColor     : COLOR0;
+    float  texcoord_4       : TEXCOORD4;
 };
 
 struct VS_OUTPUT {
-    float4 color_0 : COLOR0;
+    float4 vColor : COLOR0;
 };
 
 // Code:
@@ -45,37 +45,36 @@ VS_OUTPUT main(VS_INPUT IN) {
 #define	compress(v)		(((v) * 0.5) + 0.5)
 #define	envreflect(i, n)	((2 * dot(i, n)) * (i)) - ((n) * dot(i, i))
 
-    float3 q1;
-    float3 q4;
     float3 r0;
-    
-    float4 normals;
-    float4 cubeMap;
-    float4 envMask;
+    float3 q1;
+    float3 vProcessedCubemap;
+    float4 vNormalMap;
+    float4 vEnvMask;
+    float4 vCubemap;
     float4 cubeCoord;
 
-    normals.rgba = tex2D(NormalMap, IN.NormalUV.xy);
-    envMask.rgba = tex2D(CustomEnvMask, IN.NormalUV.xy);
-    q1.xyz = normalize(expand(normals.rgb));
+    vNormalMap.rgba = tex2D(NormalMap, IN.tcCoord.xy);
+    vEnvMask.rgba = tex2D(CustomEnvMask, IN.tcCoord.xy);
+    q1.xyz = normalize(expand(vNormalMap.rgb));
     r0.z = dot(q1.xyz, IN.texcoord_3.xyz);
     r0.y = dot(q1.xyz, IN.texcoord_2.xyz);
     r0.x = dot(q1.xyz, IN.texcoord_1.xyz);
 
-    cubeMap.z = -IN.texcoord_3.w;
-    cubeMap.y = -IN.texcoord_2.w;
-    cubeMap.x = -IN.texcoord_1.w;
+    vCubemap.z = -IN.texcoord_3.w;
+    vCubemap.y = -IN.texcoord_2.w;
+    vCubemap.x = -IN.texcoord_1.w;
 
-    cubeCoord.xyz = envreflect(normalize(r0.xyz), normalize(cubeMap.xyz));
-    cubeCoord.a = lerp(0, 5, envMask.a);
-    cubeMap.rgba = texCUBElod(EnvironmentCubeMap, cubeCoord);
+    cubeCoord.xyz = envreflect(normalize(r0.xyz), normalize(vCubemap.xyz));
+    cubeCoord.a = lerp(0, 5, vEnvMask.a);
+    vCubemap.rgba = texCUBElod(EnvironmentCubeMap, cubeCoord);
 
     // EnvToggles.w is 1 if mask is present
     // EnvToggles.z is env map scale
     // AmbientColor.a is fade
-    q4.rgb = (cubeMap.rgb * (lerp(normals.a, envMask.r, EnvToggles.w) * EnvToggles.z)) * AmbientColor.rgba;
+    vProcessedCubemap.rgb = (vCubemap.rgb * (lerp(vNormalMap.a, vEnvMask.x, EnvToggles.w) * EnvToggles.z)) * AmbientColor.rgba;
 
-    OUT.color_0.a = 1;
-    OUT.color_0.rgb = ((EnvToggles.x <= 0.0 ? q4.rgb : (q4.rgb * IN.VertexColor.rgb)) * IN.texcoord_4.x);
+    OUT.vColor.a = 1;
+    OUT.vColor.rgb = ((EnvToggles.x <= 0.0 ? vProcessedCubemap.rgb : (vProcessedCubemap.rgb * IN.VertexColor.rgb)) * IN.texcoord_4.x);
 
     return OUT;
 };
--- DynamicReflections/internal/Shaders/SM3024.pso.hlsl
+++ DynamicReflections/internal/Shaders/SM3024.pso.hlsl
@@ -2,11 +2,11 @@
 //
 // Parameters:
 
-sampler2D CustomEnvMask : register(s3);
-float4 EnvToggles : register(c11);
+sampler2D   NormalMap : register(s0);
 samplerCUBE EnvironmentCubeMap : register(s1);
-float4 MatAlpha : register(c3);
-sampler2D NormalMap : register(s0);
+sampler2D   CustomEnvMask : register(s3);
+float4      MatAlpha : register(c3);
+float4      EnvToggles : register(c11);
 
 
 // Registers:
@@ -24,16 +24,16 @@ sampler2D NormalMap : register(s0);
 // Structures:
 
 struct VS_INPUT {
-    float4 texcoord_1 : TEXCOORD1_centroid;
-    float4 texcoord_2 : TEXCOORD2_centroid;
-    float4 texcoord_3 : TEXCOORD3_centroid;
-    float2 NormalUV : TEXCOORD0;
-    float3 VertexColor : COLOR0;
-    float texcoord_4 : TEXCOORD4;
+    float4 texcoord_1       : TEXCOORD1_centroid;
+    float4 texcoord_2       : TEXCOORD2_centroid;
+    float4 texcoord_3       : TEXCOORD3_centroid;
+    float2 tcCoord          : TEXCOORD0;
+    float3 vVertexColor     : COLOR0;
+    float  texcoord_4       : TEXCOORD4;
 };
 
 struct VS_OUTPUT {
-    float4 color_0 : COLOR0;
+    float4 vColor : COLOR0;
 };
 
 // Code:
@@ -45,40 +45,35 @@ VS_OUTPUT main(VS_INPUT IN) {
 #define	compress(v)		(((v) * 0.5) + 0.5)
 #define	envreflect(i, n)	((2 * dot(i, n)) * (i)) - ((n) * dot(i, i))
 
-    const float4 const_0 = {-0.5, 0.5, 1, 0};
-
-    float4 normals;
+    float3 r0;
     float3 q1;
-    float3 q35;
-    float3 q5;
-    float4 cubeMap;
-    float4 envMask;
+    float3 vProcessedCubemap;
     float4 cubeCoord;
+    float4 vNormalMap;
+    float4 vEnvMask;
+    float4 vCubemap;
+    
 
-    normals.xyzw = tex2D(NormalMap, IN.NormalUV.xy);
-    q1.xyz = normalize(normalize(expand(normals.xyz)) * const_0.yyz);
-    envMask.xyz = normalize(float3(IN.texcoord_1.w, IN.texcoord_2.w, IN.texcoord_3.w));
-    cubeMap.z = dot(q1.xyz, normalize(IN.texcoord_3.xyz));
-    cubeMap.y = dot(q1.xyz, normalize(IN.texcoord_2.xyz));
-    cubeMap.x = dot(q1.xyz, normalize(IN.texcoord_1.xyz));
-    q35.xyz = normalize(cubeMap.xyz);
+    vNormalMap.rgba = tex2D(NormalMap, IN.tcCoord.xy);
+    vEnvMask.rgba = tex2D(CustomEnvMask, IN.tcCoord.xy);
+    q1.xyz = normalize(expand(vNormalMap.rgb));
+    vCubemap.xyz = float3(IN.texcoord_1.w, IN.texcoord_2.w, IN.texcoord_3.w);
 
-    envMask.xyzw = tex2D(CustomEnvMask, IN.NormalUV.xy);
+    r0.z = dot(q1.xyz, IN.texcoord_3.xyz);
+    r0.y = dot(q1.xyz, IN.texcoord_2.xyz);
+    r0.x = dot(q1.xyz, IN.texcoord_1.xyz);
 
-    cubeCoord.rgb = ((2 * dot(q35.xyz, envMask.xyz)) * q35.xyz) - (envMask.xxyz * dot(q35.xyz, q35.xyz));
-    cubeCoord.a = lerp(0, 5, envMask.a);
-
-    cubeMap.xyzw = texCUBElod(EnvironmentCubeMap, cubeCoord);
+    cubeCoord.rgb = envreflect(normalize(r0.xyz), normalize(vCubemap.xyz));
+    cubeCoord.a = lerp(0, 5, vEnvMask.a);
+    vCubemap.rgba = texCUBElod(EnvironmentCubeMap, cubeCoord);
 
     // EnvToggles.w is 1 if mask is present
     // EnvToggles.z is env map scale
-    // AmbientColor.a is fade
-    q5.rgb = ((cubeMap.rgb * (lerp(normals.w, envMask.x, EnvToggles.w) * EnvToggles.z)) * MatAlpha.x) * IN.VertexColor.rgb;
+    // MatAlpha.r is fade
+    vProcessedCubemap.rgb = (vCubemap.rgb * (lerp(vNormalMap.a, vEnvMask.x, EnvToggles.w) * EnvToggles.z)) * MatAlpha.r;
 
-    OUT.color_0.a = 1;
-    OUT.color_0.rgb = q5.rgb * IN.texcoord_4.x;
+    OUT.vColor.a = 1;
+    OUT.vColor.rgb = ((EnvToggles.x <= 0.0 ? vProcessedCubemap.rgb : (vProcessedCubemap.rgb * IN.vVertexColor.rgb)) * IN.texcoord_4.x);
 
     return OUT;
 };
-
-// approximately 45 instruction slots used (3 texture, 42 arithmetic)
